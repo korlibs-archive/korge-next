ea8cd8bf59919fda7e360fe0ad86ddd551d4b44f
soywiz <soywiz@gmail.com>
2020-07-15 15:10:33 +0200

    Reproduces Kotlin 1.4 regression bug
    > Task :korte:compileKotlinJvm FAILED
    e: /home/soywiz/projects/korlibs/korge-next/korte/src/commonMain/kotlin/com/soywiz/korte/Templates.kt: (40, 4): Suspend function 'invoke' should be called only from a coroutine or another suspend function
    
    FAILURE: Build failed with an exception.
    
    * What went wrong:
    Execution failed for task ':korte:compileKotlinJvm'.
    > Compilation error. See log for more details

--- korte/src/commonMain/kotlin/com/soywiz/korte/Templates.kt
+++ korte/src/commonMain/kotlin/com/soywiz/korte/Templates.kt
@@ -25,6 +25,23 @@ open class Templates(
         else -> callback()
     }
 
+	// @TODO: Kotlin 1.4-M3 regression bug
+	//> Task :korte:compileKotlinJvm FAILED
+	//e: /home/soywiz/projects/korlibs/korge-next/korte/src/commonMain/kotlin/com/soywiz/korte/Templates.kt: (37, 4): Suspend function 'invoke' should be called only from a coroutine or another suspend function
+	internal class Demo() {
+		suspend operator fun <T> invoke(name: String, block: suspend () -> T): T {
+			TODO()
+		}
+	}
+
+	suspend fun demo(callback: suspend () -> Unit) = when {
+		true -> {
+			val demo = Demo()
+			demo("test") { callback() }
+		}
+		else -> TODO()
+	}
+
     open suspend fun getInclude(name: String): Template = cache("include/$name") {
         Template(name, this@Templates, includes.newGetSure(name), config).init()
     }
