7bd7e2fba56263d279d105d7b7e2e04ebe3a35cb
adam.sernheim <adam@sernheim.com>
2020-10-08 16:06:16 +0200

    Default methods for AttachmentLoader.kt String compare against previous execution of skeleton animation

--- korge-spine/src/commonTest/kotlin/com/esotericsoftware/spine/BonePositionTest.kt
+++ korge-spine/src/commonTest/kotlin/com/esotericsoftware/spine/BonePositionTest.kt
@@ -7,51 +7,31 @@ import com.soywiz.korio.async.suspendTest
 import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korio.util.OS
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 
 class BonePositionTest {
     @Test
     fun test() = suspendTest({ !OS.isJs }) {
+        var testResult = resourcesVfs["BonePositionTest.txt"].readString().lines()
+
         val resource = resourcesVfs["spineboy/spineboy-ess.json"]
-        val json = SkeletonJson(AttachmentLoaderTest())
+        val json = SkeletonJson(object : AttachmentLoader { })
         val skeletonData = json.readSkeletonData(resource)
         val skeleton = Skeleton(skeletonData)
         val bone = skeleton.findBone("gun-tip")
         val fps = 1 / 15f
+        var line = 0
         for (animation in skeletonData.animations) {
             var time = 0f
             while (time < animation.duration) {
                 animation.apply(skeleton, time, time, false, null, 1f, MixBlend.first, MixDirection.`in`)
                 skeleton.updateWorldTransform()
-                println(animation.name + "," + bone!!.worldX + "," + bone.worldY + "," + bone.worldRotationX)
+                assertEquals(testResult[line++],"${animation.name},${bone!!.worldX},${bone.worldY},${bone.worldRotationX}")
+                println("${animation.name},${bone!!.worldX},${bone.worldY},${bone.worldRotationX}")
                 time += fps
             }
         }
     }
 }
 
-class AttachmentLoaderTest: AttachmentLoader {
-    override fun newRegionAttachment(skin: Skin, name: String, path: String): RegionAttachment? {
-        return null
-    }
-
-    override fun newMeshAttachment(skin: Skin, name: String, path: String): MeshAttachment? {
-        return null
-    }
-
-    override fun newBoundingBoxAttachment(skin: Skin, name: String): BoundingBoxAttachment? {
-        return null
-    }
-
-    override fun newClippingAttachment(skin: Skin, name: String): ClippingAttachment? {
-        return null
-    }
-
-    override fun newPathAttachment(skin: Skin, name: String): PathAttachment? {
-        return null
-    }
-
-    override fun newPointAttachment(skin: Skin, name: String): PointAttachment? {
-        return null
-    }
-}
