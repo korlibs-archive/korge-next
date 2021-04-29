779b90e60a09c4f71d6139777f168f1b5e025083
Marko Koschak <marko.koschak@tisno.de>
2021-04-29 21:20:45 +0200

    Add unit test for watch function in resourcesVfs
    The added "watch" unit test should pass since the watch call of
    resourcesVfs is inside a launch lambda. Currently it fails because of a
    bug in LocalVfsJvm.kt (most probably...).

--- korio/src/commonTest/kotlin/com/soywiz/korio/vfs/ResourcesVfsTest.kt
+++ korio/src/commonTest/kotlin/com/soywiz/korio/vfs/ResourcesVfsTest.kt
@@ -4,25 +4,37 @@ import com.soywiz.korio.async.*
 import com.soywiz.korio.file.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korio.util.OS
-import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.toList
 import kotlin.test.*
 
 class ResourcesVfsTest {
-	@Test
-	fun name() = suspendTest({ OS.isJvm }) {
-		println("[A]")
-		val listing = resourcesVfs["tresfolder"].list()
-		println("[B]")
+    @Test
+    fun name() = suspendTest({ OS.isJvm }) {
+        println("[A]")
+        val listing = resourcesVfs["tresfolder"].list()
+        println("[B]")
 
-		for (v in resourcesVfs["tresfolder"].list().filter { it.extensionLC == "txt" }.toList()) {
-			println(v)
-		}
+        for (v in resourcesVfs["tresfolder"].list().filter { it.extensionLC == "txt" }.toList()) {
+            println(v)
+        }
 
-		assertEquals(
-			"[a.txt, b.txt]",
-			resourcesVfs["tresfolder"].list().filter { it.extensionLC == "txt" }.toList().map { it.baseName }.sorted().toString()
-		)
-	}
+        assertEquals(
+            "[a.txt, b.txt]",
+            resourcesVfs["tresfolder"].list().filter { it.extensionLC == "txt" }.toList().map { it.baseName }.sorted()
+                .toString()
+        )
+    }
+
+    @Test
+    fun watch() = suspendTest({ OS.isJvm }) {
+
+        println("watcher start")
+        launch {
+            resourcesVfs["tresfolder"].watch {
+                println("Watch: $it")
+            }
+        }
+        println("watcher end")
+    }
 }
