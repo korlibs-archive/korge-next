737cd0e62de72fbb845b621451df17601a3fc4e7
soywiz <soywiz@gmail.com>
2021-05-01 21:35:27 +0200

    Update gestures sample

--- samples/gestures/src/commonMain/kotlin/main.kt
+++ samples/gestures/src/commonMain/kotlin/main.kt
@@ -23,6 +23,10 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
         }
     }
 
+    text("Zoom and rotate with two fingers")
+
+    //addFixedUpdater(2.timesPerSecond) { println(views.input.activeTouches) }
+
     touch {
         var startImageRatio = 1.0
         var startRotation = 0.degrees
