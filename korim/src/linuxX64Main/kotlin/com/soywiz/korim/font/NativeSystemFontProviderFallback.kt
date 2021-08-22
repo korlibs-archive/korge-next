9a1747305208150bbbeeb63e54eccd8eba91d70a
Ryan Loebs <obsidianx@gmail.com>
2021-08-21 20:22:10 -0700

    Add SDL2 support in Linux

--- korim/src/linuxX64Main/kotlin/com/soywiz/korim/font/NativeSystemFontProviderFallback.kt
+++ korim/src/linuxX64Main/kotlin/com/soywiz/korim/font/NativeSystemFontProviderFallback.kt
@@ -1,6 +1,3 @@
 package com.soywiz.korim.font
 
-import kotlin.native.concurrent.*
-
-@ThreadLocal
 actual val nativeSystemFontProvider: NativeSystemFontProvider = FolderBasedNativeSystemFontProvider()
