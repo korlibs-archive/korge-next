ff48092fbcb4fb5a12aaf3783884b1b18e09759a
Sam Doward <sam.doward@gmail.com>
2021-12-29 11:52:37 +0100

    Add test case for datetime formatting

--- klock/src/commonTest/kotlin/com/soywiz/klock/DateFormatTest.kt
+++ klock/src/commonTest/kotlin/com/soywiz/klock/DateFormatTest.kt
@@ -8,4 +8,12 @@ class DateFormatTest {
         assertEquals("Sat, 03 Dec 2011 10:15:30 GMT+0100", DateFormat.FORMAT1.parse("2011-12-03T10:15:30+01:00").toStringDefault())
         assertEquals("Sat, 03 Dec 2011 10:15:30 GMT+0100", DateFormat.FORMAT1.parse("2011-12-03T10:15:30+0100").toStringDefault())
     }
+
+    @Test
+    fun testTimeStampFormatting() {
+        val timeStamp01012020 = 1577836800000
+        val formattedTimeStamp = DateFormat("YYYY-MM-dd hh:mm:ss").format(timeStamp01012020)
+
+        assertEquals("2020-01-01 00:00:00", formattedTimeStamp)
+    }
 }
