5b1b37043885ddf74781322efa6a160418855e33
Soywiz InVideo <soywiz@invideo.io>
2022-04-05 18:02:13 +0200

    Fix Scissor.combine when no intersection was found

--- korgw/src/commonTest/kotlin/com/soywiz/korag/AGTest.kt
+++ korgw/src/commonTest/kotlin/com/soywiz/korag/AGTest.kt
@@ -17,5 +17,7 @@ class AGTest {
         assertEquals(null, AG.Scissor.combine(null, null))
         assertEquals(AG.Scissor(0.0, 0.0, 100.0, 100.0), AG.Scissor.combine(AG.Scissor(0.0, 0.0, 100.0, 100.0), null))
         assertEquals(AG.Scissor(50.0, 50.0, 50.0, 50.0), AG.Scissor.combine(AG.Scissor(0.0, 0.0, 100.0, 100.0), AG.Scissor(50.0, 50.0, 100.0, 100.0)))
+        assertEquals(AG.Scissor(50.0, 50.0, 100.0, 100.0), AG.Scissor.combine(null, AG.Scissor(50.0, 50.0, 100.0, 100.0)))
+        assertEquals(AG.Scissor(0.0, 0.0, 0.0, 0.0), AG.Scissor.combine(AG.Scissor(2000.0, 2000.0, 100.0, 100.0), AG.Scissor(50.0, 50.0, 100.0, 100.0)))
     }
 }
