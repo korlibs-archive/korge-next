fa3edeef1430fd99647f151eae9ca2331fd2c270
Soywiz InVideo <soywiz@invideo.io>
2022-04-05 17:45:06 +0200

    Add test for AG.Scissor.combine

--- korgw/src/commonTest/kotlin/com/soywiz/korag/AGTest.kt
+++ korgw/src/commonTest/kotlin/com/soywiz/korag/AGTest.kt
@@ -11,4 +11,11 @@ class AGTest {
 		val buffer = ag.createIndexBuffer()
 		buffer.upload(intArrayOf(1, 2, 3, 4))
 	}
+
+    @Test
+    fun testCombineScissor() {
+        assertEquals(null, AG.Scissor.combine(null, null))
+        assertEquals(AG.Scissor(0.0, 0.0, 100.0, 100.0), AG.Scissor.combine(AG.Scissor(0.0, 0.0, 100.0, 100.0), null))
+        assertEquals(AG.Scissor(50.0, 50.0, 50.0, 50.0), AG.Scissor.combine(AG.Scissor(0.0, 0.0, 100.0, 100.0), AG.Scissor(50.0, 50.0, 100.0, 100.0)))
+    }
 }
