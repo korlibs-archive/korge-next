02fe5dbb62431f6610016d03798a872871645167
soywiz <soywiz@gmail.com>
2022-04-15 14:50:33 +0200

    Some work on posixExec

--- korio/src/darwinMain/kotlin/com/soywiz/korio/process/ProcessApple.kt
+++ korio/src/darwinMain/kotlin/com/soywiz/korio/process/ProcessApple.kt
@@ -1,33 +1,83 @@
 package com.soywiz.korio.process
 
+import com.soywiz.kds.concurrent.*
+import com.soywiz.klock.*
 import com.soywiz.kmem.*
+import com.soywiz.korio.async.*
 import com.soywiz.korio.file.VfsProcessHandler
 import com.soywiz.korio.lang.*
 import com.soywiz.korio.stream.*
 import kotlinx.cinterop.*
 import platform.posix.*
-
-private fun escapeshellarg(str: String) = "'" + str.replace("'", "\\'") + "'"
+import kotlin.native.concurrent.*
 
 // @TODO: Use a separate thread
 actual suspend fun posixExec(
     path: String, cmdAndArgs: List<String>, env: Map<String, String>, handler: VfsProcessHandler
-): Int = memScoped {
-    val command = "/bin/sh -c '" + cmdAndArgs.joinToString(" ") { escapeshellarg(it) }.replace("'", "\"'\"") + "' 2>&1"
-    println("COMMAND: $command")
-    val fp = popen(command, "r")
-        ?: error("Couldn't exec ${cmdAndArgs.toList()}")
+): Int {
+    class Info(
+        val commandLine: String,
+        val stdout: ConcurrentDeque<ByteArray>,
+        val stderr: ConcurrentDeque<ByteArray>,
+        val result: ConcurrentDeque<Int>,
+    )
 
-    val temp = ByteArray(1024)
-    temp.usePinned { tempPin ->
-        do {
-            val res = fread(tempPin.startAddressOf, 1.convert(), temp.size.convert(), fp).toInt()
-            if (res > 0) {
-                handler.onOut(temp.copyOf(res))
+    println("@WARNING: this exec implementation is not setting env variables, or the current path, and not reading stderr")
+
+    // @TODO: place environment variables like ENV=value ENV2=value2 cd path; command
+    // @TODO: does it work on windows? only posix?
+    val commandLine = "/bin/sh -c '" + cmdAndArgs.joinToString(" ") { escapeshellarg(it) }.replace("'", "\"'\"") + "'"
+
+    //println("[MAIN] BEFORE WORKER: commandLine=$commandLine")
+
+    val stdoutQueue = ConcurrentDeque<ByteArray>()
+    val stderrQueue = ConcurrentDeque<ByteArray>()
+    val resultQueue = ConcurrentDeque<Int>()
+    val worker = Worker.start()
+    worker.execute(TransferMode.SAFE, { Info(commandLine, stdoutQueue, stderrQueue, resultQueue) }, { info ->
+        memScoped {
+            val f = popen(info.commandLine, "r")
+            //println("[WORKER] OPENED ${info.commandLine}")
+            val temp = ByteArray(1024)
+            temp.usePinned { pin ->
+                val tempAddress = pin.addressOf(0)
+                while (true) {
+                    val result = fread(tempAddress, 1, temp.size.convert(), f).toInt()
+                    //println("[WORKER] fread result $result")
+                    if (result <= 0) break
+                    info.stdout.add(temp.copyOf(result))
+                }
             }
-        } while (res > 0)
+            val exitCode = pclose(f)
+            //println("[WORKER] pclose $exitCode")
+            info.result.add(exitCode)
+        }
+    })
 
+    var exitCode: Int? = null
+
+    //println("[MAIN] START WAIT")
+
+    while (exitCode == null) {
+        stdoutQueue.consume()?.let {
+            //println("[MAIN] ON OUT: ${it.size}")
+            handler.onOut(it)
+        }
+        stderrQueue.consume()?.let {
+            //println("[MAIN] ON ERR: ${it.size}")
+            handler.onErr(it)
+        }
+        exitCode = resultQueue.consume()
+        if (exitCode != null) {
+            //println("[MAIN] ON EXIT: $exitCode")
+        }
+        delay(1.milliseconds)
     }
 
-    return pclose(fp)
+    //println("[MAIN] END WAIT")
+
+    worker.requestTermination()
+
+    //println("[MAIN] END WAIT 2")
+    return exitCode
 }
--- korio/src/nativeMain/kotlin/com/soywiz/korio/process/ProcessNative.kt
+++ korio/src/nativeMain/kotlin/com/soywiz/korio/process/ProcessNative.kt
@@ -4,6 +4,9 @@ import com.soywiz.korio.file.VfsProcessHandler
 import kotlinx.cinterop.*
 import platform.posix.*
 
+// @TODO: Should we escape with ^ on windows? Check
+internal fun escapeshellarg(str: String) = "'" + str.replace("'", "\\'") + "'"
+
 expect suspend fun posixExec(
     path: String, cmdAndArgs: List<String>, env: Map<String, String>, handler: VfsProcessHandler
 ): Int
