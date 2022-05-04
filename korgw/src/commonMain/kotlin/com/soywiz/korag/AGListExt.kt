34f2786e4fa626f9a557c36698c6366a64eae102
soywiz-invideo <soywiz@invideo.io>
2022-05-04 20:50:14 +0200

    Improve AGList utilities

--- korgw/src/commonMain/kotlin/com/soywiz/korag/AGListExt.kt
+++ korgw/src/commonMain/kotlin/com/soywiz/korag/AGListExt.kt
@@ -3,21 +3,21 @@ package com.soywiz.korag
 import com.soywiz.kds.*
 import com.soywiz.kmem.*
 import com.soywiz.korag.shader.*
-import com.soywiz.korio.annotations.*
 import com.soywiz.korma.geom.*
 
-@KorIncomplete fun AGList.enableBlend(): Unit = enable(AGEnable.BLEND)
-@KorIncomplete fun AGList.enableCullFace(): Unit = enable(AGEnable.CULL_FACE)
-@KorIncomplete fun AGList.enableDepth(): Unit = enable(AGEnable.DEPTH)
-@KorIncomplete fun AGList.enableScissor(): Unit = enable(AGEnable.SCISSOR)
-@KorIncomplete fun AGList.enableStencil(): Unit = enable(AGEnable.STENCIL)
-@KorIncomplete fun AGList.disableBlend(): Unit = disable(AGEnable.BLEND)
-@KorIncomplete fun AGList.disableCullFace(): Unit = disable(AGEnable.CULL_FACE)
-@KorIncomplete fun AGList.disableDepth(): Unit = disable(AGEnable.DEPTH)
-@KorIncomplete fun AGList.disableScissor(): Unit = disable(AGEnable.SCISSOR)
-@KorIncomplete fun AGList.disableStencil(): Unit = disable(AGEnable.STENCIL)
+fun AGList.enableBlend(): Unit = enable(AGEnable.BLEND)
+fun AGList.enableCullFace(): Unit = enable(AGEnable.CULL_FACE)
+fun AGList.enableDepth(): Unit = enable(AGEnable.DEPTH)
+fun AGList.enableScissor(): Unit = enable(AGEnable.SCISSOR)
+fun AGList.enableStencil(): Unit = enable(AGEnable.STENCIL)
+fun AGList.disableBlend(): Unit = disable(AGEnable.BLEND)
+fun AGList.disableCullFace(): Unit = disable(AGEnable.CULL_FACE)
+fun AGList.disableDepth(): Unit = disable(AGEnable.DEPTH)
+fun AGList.disableScissor(): Unit = disable(AGEnable.SCISSOR)
+fun AGList.disableStencil(): Unit = disable(AGEnable.STENCIL)
 
-fun AGList.setBlendingState(blending: AG.Blending = AG.Blending.NORMAL) {
+fun AGList.setBlendingState(blending: AG.Blending? = null) {
+    val blending = blending ?: AG.Blending.NORMAL
     enableDisable(AGEnable.BLEND, blending.enabled) {
         blendEquation(blending.eqRGB, blending.eqA)
         blendFunction(blending.srcRGB, blending.dstRGB, blending.srcA, blending.dstA)
@@ -37,12 +37,12 @@ fun AGList.setRenderState(renderState: AG.RenderState) {
     }
 }
 
-fun AGList.setColorMaskState(colorMask: AG.ColorMaskState) {
-    colorMask(colorMask.red, colorMask.green, colorMask.blue, colorMask.alpha)
+fun AGList.setColorMaskState(colorMask: AG.ColorMaskState?) {
+    colorMask(colorMask?.red ?: true, colorMask?.green ?: true, colorMask?.blue ?: true, colorMask?.alpha ?: true)
 }
 
-fun AGList.setStencilState(stencil: AG.StencilState) {
-    if (stencil.enabled) {
+fun AGList.setStencilState(stencil: AG.StencilState?) {
+    if (stencil != null && stencil.enabled) {
         enable(AGEnable.STENCIL)
         stencilFunction(stencil.compareMode, stencil.referenceValue, stencil.readMask)
         stencilOperation(
@@ -116,6 +116,10 @@ fun AGList.setState(
     setRenderState(renderState)
     setColorMaskState(colorMask)
     setStencilState(stencil)
+}
+
+inline fun AGList.useProgram(ag: AG, program: Program) {
+    useProgram(ag.getProgram(program))
 }
 
 inline fun AGList.uniformsSet(uniforms: AG.UniformValues, block: () -> Unit) {
