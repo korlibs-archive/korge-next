605e03e33b5ff132eedcfa4a48c13ea561a16015
soywiz-invideo <soywiz@invideo.io>
2022-05-04 20:48:32 +0200

    Added AgBufferManager to handle temporal GPU buffers that are GC'ed, analog to AgBitmapTextureManager

--- korge/src/commonMain/kotlin/com/soywiz/korge/render/AgBitmapTextureManager.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/render/AgBitmapTextureManager.kt
@@ -22,27 +22,9 @@ import com.soywiz.korma.geom.Rectangle
 class AgBitmapTextureManager(
     val ag: AG
 ) {
-    // @TODO: Use HashSet if items.size increases
-    internal class SmallSet<T> {
-        val items = FastArrayList<T>()
 
-        fun add(item: T) {
-            if (item in items) return
-            items.add(item)
-        }
 
-        fun remove(item: T) {
-            items.remove(item)
-        }
-
-        inline operator fun contains(item: T): Boolean = item in items
-
-        fun clear() {
-            items.clear()
-        }
-    }
-
-	private val referencedBitmapsSinceGC = SmallSet<Bitmap>()
+	private val referencedBitmapsSinceGC = AgFastSet<Bitmap>()
 	private var referencedBitmaps = FastArrayList<Bitmap>()
 
     /** Number of frames between each Texture Garbage Collection step */
