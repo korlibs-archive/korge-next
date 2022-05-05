30f165825bcfd0543be878d6d8cae41a8d11615e
soywiz-invideo <soywiz@invideo.io>
2022-05-05 13:03:29 +0200

    Fix some new errors

--- kbox2d/src/commonMain/kotlin/org/jbox2d/dynamics/contacts/PositionSolverManifold.kt
+++ kbox2d/src/commonMain/kotlin/org/jbox2d/dynamics/contacts/PositionSolverManifold.kt
@@ -99,6 +99,9 @@ internal class PositionSolverManifold {
                 normal.x *= -1f
                 normal.y *= -1f
             }
+
+            else -> {
+            }
         }
     }
 }
