51befa141a93a20dbafd0a0b06b6d1afa7e07aed
soywiz <soywiz@gmail.com>
2022-05-07 20:11:51 +0200

    More simplifications

--- krypto/src/commonMain/kotlin/com/soywiz/krypto/CipherMode.kt
+++ krypto/src/commonMain/kotlin/com/soywiz/krypto/CipherMode.kt
@@ -24,40 +24,9 @@ interface CipherMode {
     fun decrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray
 }
 
-private abstract class CipherModeBase(override val name: String) : CipherMode {
-    override fun toString(): String = name
-}
-
-private abstract class CipherModeCore(name: String) : CipherModeBase(name) {
-    final override fun encrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
-        val ivb = getIV(iv, cipher.blockSize)
-        val pData = padding.add(data, cipher.blockSize)
-        coreEncrypt(pData, cipher, ivb)
-        return pData
-    }
-
-    final override fun decrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
-        val ivb = getIV(iv, cipher.blockSize)
-        val pData = data.copyOf()
-        coreDecrypt(pData, cipher, ivb)
-        return padding.remove(pData)
-    }
-
-    protected abstract fun coreEncrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray)
-    protected abstract fun coreDecrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray)
-}
-
-private abstract class CipherModeCoreDE(name: String) : CipherModeCore(name) {
-    final override fun coreEncrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
-        core(pData, cipher, ivb)
-    }
-
-    final override fun coreDecrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
-        core(pData, cipher, ivb)
-    }
-
-    protected abstract fun core(pData: ByteArray, cipher: Cipher, ivb: ByteArray)
-}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 private object CipherModeECB : CipherModeBase("ECB") {
     override fun encrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
@@ -72,7 +41,7 @@ private object CipherModeECB : CipherModeBase("ECB") {
     }
 }
 
-private object CipherModeCBC : CipherModeCore("CBC") {
+private object CipherModeCBC : CipherModeIV("CBC") {
     override fun coreEncrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
         for (n in pData.indices step cipher.blockSize) {
             arrayxor(pData, n, ivb)
@@ -94,41 +63,22 @@ private object CipherModeCBC : CipherModeCore("CBC") {
     }
 }
 
-private object CipherModePCBC : CipherModeBase("PCBC") {
-    override fun encrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
-        val pData = padding.add(data, cipher.blockSize)
-        val ivWords = getIV(iv, cipher.blockSize).toIntArray()
-        val plaintext = IntArray(4)
+private object CipherModePCBC : CipherModeIV("PCBC") {
+    override fun coreEncrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
+        val blockSize = cipher.blockSize
+        val plaintext = ByteArray(blockSize)
 
-        var s0 = ivWords[0]
-        var s1 = ivWords[1]
-        var s2 = ivWords[2]
-        var s3 = ivWords[3]
-
-        for (n in pData.indices step cipher.blockSize) {
-            plaintext[0] = pData.getInt(n + 0 * 4)
-            plaintext[1] = pData.getInt(n + 1 * 4)
-            plaintext[2] = pData.getInt(n + 2 * 4)
-            plaintext[3] = pData.getInt(n + 3 * 4)
-
-            pData.setInt(n + 0 * 4, plaintext[0] xor s0)
-            pData.setInt(n + 1 * 4, plaintext[1] xor s1)
-            pData.setInt(n + 2 * 4, plaintext[2] xor s2)
-            pData.setInt(n + 3 * 4, plaintext[3] xor s3)
-
+        for (n in pData.indices step blockSize) {
+            arraycopy(pData, n, plaintext, 0, blockSize)
+            arrayxor(pData, n, ivb)
             cipher.encrypt(pData, n, cipher.blockSize)
-
-            s0 = pData.getInt(n + 0 * 4) xor plaintext[0]
-            s1 = pData.getInt(n + 1 * 4) xor plaintext[1]
-            s2 = pData.getInt(n + 2 * 4) xor plaintext[2]
-            s3 = pData.getInt(n + 3 * 4) xor plaintext[3]
+            arraycopy(pData, n, ivb, 0, blockSize)
+            arrayxor(ivb, 0, plaintext)
         }
-        return pData
     }
 
-    override fun decrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
-        val cdata = data.copyOf()
-        val ivWords = getIV(iv, cipher.blockSize).toIntArray()
+    override fun coreDecrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
+        val ivWords = ivb.toIntArray()
         val cipherText = IntArray(4)
 
         var s0 = ivWords[0]
@@ -136,25 +86,24 @@ private object CipherModePCBC : CipherModeBase("PCBC") {
         var s2 = ivWords[2]
         var s3 = ivWords[3]
 
-        for (n in cdata.indices step cipher.blockSize) {
-            cipherText[0] = cdata.getInt(n + 0 * 4)
-            cipherText[1] = cdata.getInt(n + 1 * 4)
-            cipherText[2] = cdata.getInt(n + 2 * 4)
-            cipherText[3] = cdata.getInt(n + 3 * 4)
+        for (n in pData.indices step cipher.blockSize) {
+            cipherText[0] = pData.getInt(n + 0 * 4)
+            cipherText[1] = pData.getInt(n + 1 * 4)
+            cipherText[2] = pData.getInt(n + 2 * 4)
+            cipherText[3] = pData.getInt(n + 3 * 4)
 
-            cipher.decrypt(cdata, n, cipher.blockSize)
+            cipher.decrypt(pData, n, cipher.blockSize)
 
-            cdata.setInt(n + 0 * 4, cdata.getInt(n + 0 * 4) xor s0)
-            cdata.setInt(n + 1 * 4, cdata.getInt(n + 1 * 4) xor s1)
-            cdata.setInt(n + 2 * 4, cdata.getInt(n + 2 * 4) xor s2)
-            cdata.setInt(n + 3 * 4, cdata.getInt(n + 3 * 4) xor s3)
+            pData.setInt(n + 0 * 4, pData.getInt(n + 0 * 4) xor s0)
+            pData.setInt(n + 1 * 4, pData.getInt(n + 1 * 4) xor s1)
+            pData.setInt(n + 2 * 4, pData.getInt(n + 2 * 4) xor s2)
+            pData.setInt(n + 3 * 4, pData.getInt(n + 3 * 4) xor s3)
 
-            s0 = cdata.getInt(n + 0 * 4) xor cipherText[0]
-            s1 = cdata.getInt(n + 1 * 4) xor cipherText[1]
-            s2 = cdata.getInt(n + 2 * 4) xor cipherText[2]
-            s3 = cdata.getInt(n + 3 * 4) xor cipherText[3]
+            s0 = pData.getInt(n + 0 * 4) xor cipherText[0]
+            s1 = pData.getInt(n + 1 * 4) xor cipherText[1]
+            s2 = pData.getInt(n + 2 * 4) xor cipherText[2]
+            s3 = pData.getInt(n + 3 * 4) xor cipherText[3]
         }
-        return padding.remove(cdata)
     }
 }
 
@@ -234,8 +183,8 @@ private object CipherModeCFB : CipherModeBase("CFB") {
     }
 }
 
-private object CipherModeOFB : CipherModeCoreDE("OFB") {
-    override fun core(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
+private object CipherModeOFB : CipherModeIVDE("OFB") {
+    override fun coreCrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
         val blockSize = cipher.blockSize
         val cipherText = ByteArray(blockSize)
         cipher.encrypt(ivb)
@@ -251,22 +200,52 @@ private object CipherModeOFB : CipherModeCoreDE("OFB") {
 }
 
 // https://github.com/Jens-G/haxe-crypto/blob/dcf6d994773abba80b0720b2f5e9d5b26de0dbe3/src/com/hurlant/crypto/symmetric/mode/CTRMode.hx
-private object CipherModeCTR : CipherModeCoreDE("CTR") {
-    override fun core(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
+private object CipherModeCTR : CipherModeIVDE("CTR") {
+    override fun coreCrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) {
         val blockSize = cipher.blockSize
         val temp = ByteArray(ivb.size)
         for (n in pData.indices step blockSize) {
             arraycopy(ivb, 0, temp, 0, temp.size)
             cipher.encrypt(temp, 0, blockSize)
             arrayxor(pData, n, temp)
-            updateIV(ivb, blockSize)
+            for (j in blockSize - 1 downTo 0) {
+                ivb[j]++
+                if (ivb[j].toInt() != 0) break
+            }
         }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+private abstract class CipherModeBase(override val name: String) : CipherMode {
+    override fun toString(): String = name
+}
+
+private abstract class CipherModeIV(name: String) : CipherModeBase(name) {
+    final override fun encrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
+        val ivb = getIV(iv, cipher.blockSize)
+        val pData = padding.add(data, cipher.blockSize)
+        coreEncrypt(pData, cipher, ivb)
+        return pData
     }
 
-    private fun updateIV(ivb: ByteArray, blockSize: Int) {
-        for (j in blockSize - 1 downTo 0) {
-            ivb[j]++
-            if (ivb[j].toInt() != 0) break
-        }
+    final override fun decrypt(data: ByteArray, cipher: Cipher, padding: Padding, iv: ByteArray?): ByteArray {
+        val ivb = getIV(iv, cipher.blockSize)
+        val pData = data.copyOf()
+        coreDecrypt(pData, cipher, ivb)
+        return padding.remove(pData)
     }
+
+    protected abstract fun coreEncrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray)
+    protected abstract fun coreDecrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray)
+}
+
+private abstract class CipherModeIVDE(name: String) : CipherModeIV(name) {
+    final override fun coreEncrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) = coreCrypt(pData, cipher, ivb)
+    final override fun coreDecrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray) = coreCrypt(pData, cipher, ivb)
+
+    protected abstract fun coreCrypt(pData: ByteArray, cipher: Cipher, ivb: ByteArray)
 }
