a1eef65003414486fe42a89bee27506167d318a0
soywiz <soywiz@gmail.com>
2022-05-07 19:23:30 +0200

    Test against JVM

---
+++ krypto/src/jvmTest/kotlin/com/soywiz/krypto/AESJvmTest.kt
@@ -0,0 +1,69 @@
+package com.soywiz.krypto
+
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AESJvmTest {
+    val MODES = listOf(
+        CipherMode.ECB,
+        CipherMode.CBC,
+        CipherMode.PCBC,
+        CipherMode.CFB,
+        CipherMode.OFB,
+        //CipherMode.CTR,
+    )
+
+    @Test
+    fun testSimple() {
+        val key = SecureRandom.nextBytes(16)
+        val iv = SecureRandom.nextBytes(16)
+        val plain = ByteArray(32) { it.toByte() }
+        val encrypted = encryptJava("AES", "CTR", "NoPadding", plain, key, iv)
+        val decrypted = decryptJava("AES", "CTR", "NoPadding", encrypted, key, iv)
+        assertEquals(plain.hex, decrypted.hex)
+    }
+
+    @Test
+    fun testCompareOurImpl() {
+        val key = SecureRandom.nextBytes(16)
+        val iv = SecureRandom.nextBytes(16)
+        val ivCopy = iv.copyOf()
+        val plain = ByteArray(32) { it.toByte() }
+        val plainCopy = plain.copyOf()
+
+        for (mode in MODES) {
+            val encryptor = AES(key)[mode, CipherPadding.NoPadding, iv]
+            val encrypted = encryptJava("AES", mode.name, "NoPadding", plain, key, iv)
+            val encryptedCopy = encrypted.copyOf()
+            val decrypted = encryptor.decrypt(encrypted)
+
+            assertEquals(encryptedCopy.hex, encrypted.hex)
+            assertEquals(plainCopy.hex, plain.hex)
+            assertEquals(ivCopy.hex, iv.hex)
+
+            assertEquals(plain.hex, decrypted.hex)
+        }
+    }
+
+    private fun encryptJava(algo: String, mode: String, padding: String, data: ByteArray, key: ByteArray = SecureRandom.nextBytes(16), iv: ByteArray = SecureRandom.nextBytes(16)): ByteArray {
+        return encryptDecryptJava(javax.crypto.Cipher.ENCRYPT_MODE, algo, mode, padding, data, key, iv)
+    }
+
+    private fun decryptJava(algo: String, mode: String, padding: String, data: ByteArray, key: ByteArray = SecureRandom.nextBytes(16), iv: ByteArray = SecureRandom.nextBytes(16)): ByteArray {
+        return encryptDecryptJava(javax.crypto.Cipher.DECRYPT_MODE, algo, mode, padding, data, key, iv)
+    }
+
+    private fun encryptDecryptJava(emode: Int, algo: String, mode: String, padding: String, data: ByteArray, key: ByteArray = SecureRandom.nextBytes(16), iv: ByteArray = SecureRandom.nextBytes(16)): ByteArray {
+        return javax.crypto.Cipher.getInstance("$algo/$mode/$padding").let { cipher ->
+            val keySpec = SecretKeySpec(key, algo)
+            if (mode == "ECB") {
+                cipher.init(emode, keySpec)
+            } else {
+                cipher.init(emode, keySpec, IvParameterSpec(iv))
+            }
+            cipher.doFinal(data)
+        }
+    }
+}
