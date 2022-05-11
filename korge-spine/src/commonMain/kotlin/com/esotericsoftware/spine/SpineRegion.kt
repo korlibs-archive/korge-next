7264de05a4aefbe1170496038f9a23f58d0a0efb
Stechmuck <stechi@famro.de>
2022-05-11 09:20:57 +0200

    Update loadText() to use new bmpCoords

--- korge-spine/src/commonMain/kotlin/com/esotericsoftware/spine/SpineRegion.kt
+++ korge-spine/src/commonMain/kotlin/com/esotericsoftware/spine/SpineRegion.kt
@@ -6,16 +6,16 @@ class SpineRegion(val entry: Atlas.Entry) {
     val bmpSlice = entry.slice
     val bmp = bmpSlice.bmpBase
     val texture = bmp
-    var rotate = entry.info.rotated
-    val u: Float = if (rotate) bmpSlice.br_x else bmpSlice.tl_x
-    val u2: Float = if (rotate) bmpSlice.tl_x else bmpSlice.br_x
-    val v: Float = bmpSlice.tl_y
-    val v2: Float = bmpSlice.br_y
+    var rotate = entry.info.bmpCoords != null
+    val u: Float = bmpSlice.tl_x
+    val u2: Float = bmpSlice.br_x
+    val v: Float = if (rotate) bmpSlice.br_y else bmpSlice.tl_y
+    val v2: Float = if (rotate) bmpSlice.tl_y else bmpSlice.br_y
     var offsetX = entry.info.offset.x.toFloat()
     var offsetY = entry.info.offset.y.toFloat()
     var originalWidth = entry.info.orig.width.toFloat()
     var originalHeight = entry.info.orig.height.toFloat()
-    var packedWidth = entry.info.sourceSize.width.toFloat()
-    var packedHeight = entry.info.sourceSize.height.toFloat()
+    var packedWidth = (if (rotate) entry.info.sourceSize.height else entry.info.sourceSize.width).toFloat()
+    var packedHeight = (if (rotate) entry.info.sourceSize.width else entry.info.sourceSize.height).toFloat()
     var degrees = if (rotate) 90 else 0
 }
