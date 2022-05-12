684563d526339de2495345b0c876125c8415b019
soywiz-invideo <soywiz@invideo.io>
2022-05-12 14:37:53 +0200

    Update AWT sandbox sample

--- korge-sandbox/src/jvmMain/kotlin/AwtSandboxSample.kt
+++ korge-sandbox/src/jvmMain/kotlin/AwtSandboxSample.kt
@@ -1,12 +1,14 @@
+import com.soywiz.kds.iterators.fastForEach
+import com.soywiz.kds.iterators.fastForEachWithIndex
 import com.soywiz.korge.GLCanvasKorge
 import com.soywiz.korge.GLCanvasWithKorge
 import com.soywiz.korge.Korge
 import com.soywiz.korge.input.onClick
 import com.soywiz.korge.jvmEnsureAddOpens
-import com.soywiz.korge.ui.UIButton
-import com.soywiz.korge.ui.uiButton
-import com.soywiz.korge.ui.uiVerticalStack
+import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.descendantsWith
+import com.soywiz.korim.color.Colors
+import com.soywiz.korim.color.toRgba
 import com.soywiz.korio.async.launchImmediately
 import com.soywiz.korma.geom.Anchor
 import com.soywiz.korma.geom.SizeInt
@@ -31,32 +33,54 @@ object AwtSandboxSample {
         frame.setLocationRelativeTo(null)
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         frame.isVisible = true
+        frame.title = "KorGE Awt Sandbox Sample"
 
         val components = arrayListOf<Component>()
 
         for (n in 0 until 9) {
-            if (n == 4) {
-                components.add(frame.add(GLCanvasWithKorge(Korge.Config(virtualSize = SizeInt(128, 128), scaleAnchor = Anchor.TOP_LEFT)) {
-                    val jbutton0 = components.first() as JButton
-                    uiVerticalStack {
-                        uiButton("${views.devicePixelRatio}")
-                        uiButton("HELLO") {
-                            name = "helloButton"
-                            onClick {
-                                SwingUtilities.invokeLater {
-                                    jbutton0.text = "hello!"
+            if (n == 4 || n == 8) {
+            //if (n == 4) {
+                components.add(
+                    frame.add(
+                        GLCanvasWithKorge(
+                            Korge.Config(
+                                bgcolor = frame.background.toRgba(),
+                                virtualSize = SizeInt(UIButton.DEFAULT_WIDTH.toInt(), UIButton.DEFAULT_HEIGHT.toInt() * 3),
+                                scaleAnchor = Anchor.CENTER
+                            )
+                        ) {
+                            uiVerticalStack {
+                                uiButton("${views.devicePixelRatio}")
+                                uiButton("${n}HELLO") {
+                                    name = "helloButton"
+                                    onClick {
+                                        SwingUtilities.invokeLater {
+                                            components.fastForEachWithIndex { index, component ->
+                                                if (component is JButton) {
+                                                    component.text = "K$index"
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
+                                uiButton("WORLD")
                             }
-                        }
-                        uiButton("WORLD")
-                    }
-                }))
+                        })
+                )
             } else {
                 components.add(frame.add(JButton("$n").also {
                     it.addActionListener {
-                        val canvas = components.filterIsInstance<GLCanvasWithKorge>().first()
-                        canvas.korge.launchInContext {
-                            (stage.findViewByName("helloButton") as UIButton).text = "YAY! $n"
+                        components.fastForEachWithIndex { index, component ->
+                            if (component is JButton) {
+                                SwingUtilities.invokeLater {
+                                    component.text = "J$index"
+                                }
+                            }
+                            if (component is GLCanvasWithKorge) {
+                                component.korge.launchInContext {
+                                    (stage.findViewByName("helloButton") as UIButton).text = "$n/$index YAY!"
+                                }
+                            }
                         }
                     }
                 }))
