78777e50054a44b8515cd71ff88b2711e172d7ca
Stechmuck <stechi@famro.de>
2022-05-14 16:59:42 +0200

    Changed primary constructor of AtlasIinfo.Region to use virtFrame and imageOrientation
    Old constructor and unused properties marked as deprecated

--- korge-spine/src/commonMain/kotlin/com/esotericsoftware/spine/SpineRegion.kt
+++ korge-spine/src/commonMain/kotlin/com/esotericsoftware/spine/SpineRegion.kt
@@ -1,21 +1,22 @@
 package com.esotericsoftware.spine
 
 import com.soywiz.korim.atlas.*
+import com.soywiz.korim.format.ImageOrientation
 
 class SpineRegion(val entry: Atlas.Entry) {
     val bmpSlice = entry.slice
     val bmp = bmpSlice.bmpBase
     val texture = bmp
-    var rotate = entry.info.textureCoords != null
+    var rotate = entry.info.imageOrientation != ImageOrientation.ORIGINAL
     val u: Float = bmpSlice.tl_x
     val u2: Float = bmpSlice.br_x
     val v: Float = if (rotate) bmpSlice.br_y else bmpSlice.tl_y
     val v2: Float = if (rotate) bmpSlice.tl_y else bmpSlice.br_y
-    var offsetX = entry.info.offset.x.toFloat()
-    var offsetY = entry.info.offset.y.toFloat()
-    var originalWidth = entry.info.orig.width.toFloat()
-    var originalHeight = entry.info.orig.height.toFloat()
-    var packedWidth = (if (rotate) entry.info.sourceSize.height else entry.info.sourceSize.width).toFloat()
-    var packedHeight = (if (rotate) entry.info.sourceSize.width else entry.info.sourceSize.height).toFloat()
+    var offsetX = (entry.info.virtFrame?.x ?: 0).toFloat()
+    var offsetY = (entry.info.virtFrame?.y ?: 0).toFloat()
+    var originalWidth = (entry.info.virtFrame?.w ?: entry.info.srcWidth).toFloat()
+    var originalHeight = (entry.info.virtFrame?.h ?: entry.info.srcHeight).toFloat()
+    var packedWidth = entry.info.srcWidth.toFloat()
+    var packedHeight = entry.info.srcHeight.toFloat()
     var degrees = if (rotate) 90 else 0
 }
--- korim/src/commonMain/kotlin/com/soywiz/korim/atlas/Atlas.kt
+++ korim/src/commonMain/kotlin/com/soywiz/korim/atlas/Atlas.kt
@@ -2,12 +2,15 @@ package com.soywiz.korim.atlas
 
 import com.soywiz.korim.bitmap.Bitmap
 import com.soywiz.korim.bitmap.BitmapSlice
+import com.soywiz.korim.bitmap.BmpCoordsWithInstance
 import com.soywiz.korim.bitmap.BmpSlice
 import com.soywiz.korim.bitmap.asumePremultiplied
 import com.soywiz.korim.bitmap.copy
 import com.soywiz.korim.format.readBitmapSlice
+import com.soywiz.korim.format.withImageOrientation
 import com.soywiz.korio.file.VfsFile
-import com.soywiz.korma.geom.RectangleInt
+import com.soywiz.korma.geom.Point
+import com.soywiz.korma.geom.SizeInt
 
 class Atlas(val textures: Map<String, BitmapSlice<Bitmap>>, val info: AtlasInfo = AtlasInfo()) : AtlasLookup {
     constructor(texture: BitmapSlice<Bitmap>, info: AtlasInfo = AtlasInfo()) : this(mapOf(info.pages.first().fileName to texture), info)
@@ -18,18 +21,23 @@ class Atlas(val textures: Map<String, BitmapSlice<Bitmap>>, val info: AtlasInfo 
     inner class Entry(val info: AtlasInfo.Region, val page: AtlasInfo.Page) {
         val texture = textures[page.fileName]
             ?: error("Can't find '${page.fileName}' in ${textures.keys}")
-        val slice = texture.slice(info.frame.rect, info.name).let {
-            // Define virtual frame with offsets "info.spriteSourceSize.x" and "info.spriteSourceSize.y"
-            // and original texture size "info.sourceSize.width" and "info.sourceSize.height"
-            it.copy(
-                rotated = info.rotated,
-                virtFrame = when {
-                    info.trimmed -> RectangleInt(info.spriteSourceSize.x, info.spriteSourceSize.y, info.sourceSize.width, info.sourceSize.height)
-                    else -> null
-                },
-                textureCoords = info.textureCoords
-            )
-        }
+        val slice = texture.slice(info.frame.toRectangleInt(), info.name)
+            .let {
+                val tl = Point(info.frame.x / page.size.width.toDouble(), info.frame.y / page.size.height.toDouble())
+                val br = Point(tl.x + info.srcWidth / page.size.width.toDouble(), tl.y + info.srcHeight / page.size.height.toDouble())
+                val tr = Point(br.x, tl.y)
+                val bl = Point(tl.x, br.y)
+                it.copy(
+                    virtFrame = info.virtFrame?.toRectangleInt(),
+                    bmpCoords = BmpCoordsWithInstance(
+                        SizeInt(),
+                        tl.x.toFloat(), tl.y.toFloat(),
+                        tr.x.toFloat(), tr.y.toFloat(),
+                        br.x.toFloat(), br.y.toFloat(),
+                        bl.x.toFloat(), bl.y.toFloat()
+                    ).withImageOrientation(info.imageOrientation)
+                )
+            }
         val name get() = info.name
         // @TODO: Use name instead
         val filename get() = info.name
