e691b02ca5cd8ae3b32700e2dc929b45ebd6c48f
Stechmuck <stechi@famro.de>
2022-05-21 10:31:26 +0200

    Tests for rotated Atlas-Textures

--- korge/src/commonTest/kotlin/com/soywiz/korge/atlas/AtlasInfoTest.kt
+++ korge/src/commonTest/kotlin/com/soywiz/korge/atlas/AtlasInfoTest.kt
@@ -1,10 +1,15 @@
 package com.soywiz.korge.atlas
 
+import com.soywiz.korim.atlas.Atlas
 import com.soywiz.korim.atlas.AtlasInfo
+import com.soywiz.korim.atlas.readAtlas
+import com.soywiz.korim.bitmap.BmpSlice
 import com.soywiz.korio.async.suspendTest
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korio.lang.assert
 import com.soywiz.korio.util.OS
 import com.soywiz.korma.geom.Rectangle
+import com.soywiz.korma.geom.RectangleInt
 import com.soywiz.korma.geom.Size
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -28,5 +33,56 @@ class AtlasInfoTest {
         assertEquals(Rectangle(8, 7, 41, 28), firstFrame.spriteSourceSize.rect)
         assertEquals(true, firstFrame.rotated)
         assertEquals(true, firstFrame.trimmed)
+    }
+
+    @Test
+    fun atlasTestXml() = suspendTest({ OS.isJvm }) {
+        testAtlas(resourcesVfs["atlas-test.xml"].readAtlas())
+    }
+
+    @Test
+    fun atlasTestJson() = suspendTest({ OS.isJvm }) {
+        testAtlas(resourcesVfs["atlas-test.json"].readAtlas(), ".png")
+    }
+
+    @Test
+    fun atlasTestTxt() = suspendTest({ OS.isJvm }) {
+        testAtlas(resourcesVfs["atlas-test-spine.atlas.txt"].readAtlas())
+    }
+
+    fun testAtlas(atlas: Atlas, ext:String = "") {
+        // On the test textures, the edges are colorized with different intensity to test the transform
+        testSlice(atlas["1$ext"], true, false, false) // red
+        testSlice(atlas["2$ext"], false, true, false) // green
+        testSlice(atlas["3$ext"], false, false, true) // blue
+        testSlice(atlas["4$ext"], true, true, false)  // yellow
+        testSlice(atlas["5$ext"], true, false, true)  // magenta
+        testSlice(atlas["6$ext"], false, true, true)  // cyan
+    }
+
+    fun testSlice(slice: BmpSlice, r: Boolean, g: Boolean, b: Boolean) {
+        when (slice.name) {
+            "1" -> assert(slice.virtFrame == null || slice.virtFrame == RectangleInt(0, 0, 32, 46), { "Slice ${slice.name} failed on virtFrame" })
+            "2" -> assert(slice.virtFrame == RectangleInt(0, 0, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "3" -> assert(slice.virtFrame == RectangleInt(10, 0, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "4" -> assert(slice.virtFrame == RectangleInt(10, 10, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "5" -> assert(slice.virtFrame == RectangleInt(0, 10, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "6" -> assert(slice.virtFrame == RectangleInt(5, 5, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+        }
+        assert(slice.width == 32, { "Slice ${slice.name} failed on width" })
+        assert(slice.height == 46, { "Slice ${slice.name} failed on height" })
+
+        for (i in 0 until 4) {
+            val col = when(i) {
+                0 -> slice.getRgba(0, 0)
+                1 -> slice.getRgba(31, 0)
+                2 -> slice.getRgba(31, 45)
+                else -> slice.getRgba(0, 45)
+            }
+            val c = 250 - i * 50
+            assert(col.r == if (r) c else 0, { "Slice ${slice.name} failed on pos $i at red" })
+            assert(col.g == if (g) c else 0, { "Slice ${slice.name} failed on pos $i at green" })
+            assert(col.b == if (b) c else 0, { "Slice ${slice.name} failed on pos $i at blue" })
+        }
     }
 }
