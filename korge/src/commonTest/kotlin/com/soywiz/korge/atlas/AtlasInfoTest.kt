50eed94311e5349dc0e59bffe240df5dcd51c6c9
Stechmuck <stechi@famro.de>
2022-05-28 17:36:40 +0200

    Change tests to support different virtFrames

--- korge/src/commonTest/kotlin/com/soywiz/korge/atlas/AtlasInfoTest.kt
+++ korge/src/commonTest/kotlin/com/soywiz/korge/atlas/AtlasInfoTest.kt
@@ -7,6 +7,7 @@ import com.soywiz.korim.bitmap.BmpSlice
 import com.soywiz.korio.async.suspendTest
 import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korio.lang.assert
+import com.soywiz.korio.lang.substr
 import com.soywiz.korio.util.OS
 import com.soywiz.korma.geom.Rectangle
 import com.soywiz.korma.geom.RectangleInt
@@ -52,32 +53,32 @@ class AtlasInfoTest {
 
     fun testAtlas(atlas: Atlas, ext:String = "") {
         // On the test textures, the edges are colorized with different intensity to test the transform
-        testSlice(atlas["1$ext"], true, false, false) // red
-        testSlice(atlas["2$ext"], false, true, false) // green
-        testSlice(atlas["3$ext"], false, false, true) // blue
-        testSlice(atlas["4$ext"], true, true, false)  // yellow
-        testSlice(atlas["5$ext"], true, false, true)  // magenta
-        testSlice(atlas["6$ext"], false, true, true)  // cyan
+        testSlice(atlas["1$ext"], true, false, false, 0, 0) // red
+        testSlice(atlas["2$ext"], false, true, false, 0, 0) // green
+        testSlice(atlas["3$ext"], false, false, true, 10, 0) // blue
+        testSlice(atlas["4$ext"], true, true, false, 10, 10)  // yellow
+        testSlice(atlas["5$ext"], true, false, true, 0, 10)  // magenta
+        testSlice(atlas["6$ext"], false, true, true, 5, 5)  // cyan
     }
 
-    fun testSlice(slice: BmpSlice, r: Boolean, g: Boolean, b: Boolean) {
-        when (slice.name) {
+    fun testSlice(slice: BmpSlice, r: Boolean, g: Boolean, b: Boolean, xOffset: Int, yOffset: Int) {
+        when (slice.name?.substr(0, 1)) {
             "1" -> assert(slice.virtFrame == null || slice.virtFrame == RectangleInt(0, 0, 32, 46), { "Slice ${slice.name} failed on virtFrame" })
-            "2" -> assert(slice.virtFrame == RectangleInt(0, 0, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
-            "3" -> assert(slice.virtFrame == RectangleInt(10, 0, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
-            "4" -> assert(slice.virtFrame == RectangleInt(10, 10, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
-            "5" -> assert(slice.virtFrame == RectangleInt(0, 10, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
-            "6" -> assert(slice.virtFrame == RectangleInt(5, 5, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "2" -> assert(slice.virtFrame == RectangleInt(xOffset, yOffset, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "3" -> assert(slice.virtFrame == RectangleInt(xOffset, yOffset, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "4" -> assert(slice.virtFrame == RectangleInt(xOffset, yOffset, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "5" -> assert(slice.virtFrame == RectangleInt(xOffset, yOffset, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
+            "6" -> assert(slice.virtFrame == RectangleInt(xOffset, yOffset, 42, 56), { "Slice ${slice.name} failed on virtFrame" })
         }
         assert(slice.width == 32, { "Slice ${slice.name} failed on width" })
         assert(slice.height == 46, { "Slice ${slice.name} failed on height" })
 
         for (i in 0 until 4) {
             val col = when(i) {
-                0 -> slice.getRgba(0, 0)
-                1 -> slice.getRgba(31, 0)
-                2 -> slice.getRgba(31, 45)
-                else -> slice.getRgba(0, 45)
+                1 -> slice.getRgba(xOffset + 31, yOffset + 0)
+                2 -> slice.getRgba(xOffset + 31, yOffset + 45)
+                3 -> slice.getRgba(xOffset + 0, yOffset + 45)
+                else -> slice.getRgba(xOffset + 0, yOffset + 0)
             }
             val c = 250 - i * 50
             assert(col.r == if (r) c else 0, { "Slice ${slice.name} failed on pos $i at red" })
