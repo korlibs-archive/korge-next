9c9a1eb3ade60cf4ef69d74e8f783d6e2ce08c28
Stechmuck <stechi@famro.de>
2022-05-28 17:33:08 +0200

    add test for read/write pixels on transformed slices

--- korim/src/commonTest/kotlin/com/soywiz/korim/bitmap/BitmapSliceTest.kt
+++ korim/src/commonTest/kotlin/com/soywiz/korim/bitmap/BitmapSliceTest.kt
@@ -1,7 +1,10 @@
 package com.soywiz.korim.bitmap
 
+import com.soywiz.korim.color.Colors
+import com.soywiz.korim.format.ImageOrientation
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class BitmapSliceTest {
     @Test
@@ -44,5 +47,66 @@ class BitmapSliceTest {
         val bmp2 = bmp.rotatedRight()
         assertEquals("128x64", bmp.sizeString)
         assertEquals("64x128", bmp2.sizeString)
+    }
+
+    @Test
+    fun testTransformed() {
+        val bmp = Bitmap32(20, 10)
+        val slice = bmp.sliceWithSize(1, 1, 8, 18, imageOrientation = ImageOrientation.ROTATE_90)
+
+        slice.setRgba(0, 0, Colors.RED)
+        assertEquals(Colors.RED, slice.getRgba(0, 0))
+        assertEquals(Colors.RED, bmp.getRgba(1, 8))
+
+        slice.flippedX()
+        assertEquals(Colors.RED, slice.getRgba(0, 0))
+        slice.flippedX()
+
+        val vfSlice = slice.virtFrame(2, 4, 12, 26)
+        assertEquals(Colors.RED, vfSlice.getRgba(2, 4))
+        assertEquals(Colors.TRANSPARENT_BLACK, vfSlice.getRgba(0, 0))
+        assertEquals(Colors.TRANSPARENT_BLACK, vfSlice.getRgba(11, 25))
+        assertFailsWith<IllegalArgumentException> { vfSlice.getRgba(0, -1) }
+        assertFailsWith<IllegalArgumentException> { vfSlice.getRgba(-1, 0) }
+        assertFailsWith<IllegalArgumentException> { vfSlice.getRgba(11, 26) }
+        assertFailsWith<IllegalArgumentException> { vfSlice.getRgba(12, 25) }
+
+        vfSlice.setRgba(11, 1,  Colors.BLUE)
+        assertEquals(Colors.BLUE, slice.getRgba(0, 0))
+        assertEquals(Colors.BLUE, bmp.getRgba(0, 0))
+    }
+
+    @Test
+    fun testReadPixels() {
+        val bmp = Bitmap32(20, 10)
+        val slice = bmp.sliceWithSize(1, 1, 8, 18, imageOrientation = ImageOrientation.ROTATE_90)
+
+        slice.setRgba(0, 0, Colors.RED)
+        slice.setRgba(7, 17, Colors.BLUE)
+        assertEquals(Colors.RED, slice.getRgba(0, 0))
+        assertEquals(Colors.RED, bmp.getRgba(1, 8))
+        assertEquals(Colors.BLUE, slice.getRgba(7, 17))
+        assertEquals(Colors.BLUE, bmp.getRgba(18, 1))
+
+        val sliceDataTopLeft = slice.readPixels(0, 0, 2, 2)
+        val sliceDataBottomRight = slice.readPixels(6, 16, 2, 2)
+        assertEquals(Colors.RED, sliceDataTopLeft[0])
+        assertEquals(Colors.BLUE, sliceDataBottomRight[3])
+
+        assertFailsWith<IllegalStateException> { slice.readPixels(-1, 0, 2, 2) }
+        assertFailsWith<IllegalStateException> { slice.readPixels(0, -1, 2, 2) }
+        assertFailsWith<IllegalStateException> { slice.readPixels(7, 16, 2, 2) }
+        assertFailsWith<IllegalStateException> { slice.readPixels(6, 17, 2, 2) }
+
+        val sliceVirtFrame = slice.virtFrame(1, 1, 10, 20)
+        val sliceVirtFrameDataTopLeft = sliceVirtFrame.readPixels(0, 0, 2, 2)
+        val sliceVirtFrameDataBottomRight = sliceVirtFrame.readPixels(8, 18, 2, 2)
+        assertEquals(Colors.RED, sliceVirtFrameDataTopLeft[3])
+        assertEquals(Colors.BLUE, sliceVirtFrameDataBottomRight[0])
+
+        assertFailsWith<IllegalStateException> { sliceVirtFrame.readPixels(-1, 0, 2, 2) }
+        assertFailsWith<IllegalStateException> { sliceVirtFrame.readPixels(0, -1, 2, 2) }
+        assertFailsWith<IllegalStateException> { sliceVirtFrame.readPixels(9, 18, 2, 2) }
+        assertFailsWith<IllegalStateException> { sliceVirtFrame.readPixels(8, 19, 2, 2) }
     }
 }
