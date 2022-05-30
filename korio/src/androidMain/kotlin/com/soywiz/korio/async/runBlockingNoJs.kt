422d9e9decf6e53b233332976574b6c2e17095a6
soywiz-invideo <soywiz@invideo.io>
2022-05-30 18:40:24 +0200

    Make runBlockingNoJs to propagate its coroutine context (but the dispatcher) so the Android context is propagated

--- korio/src/androidMain/kotlin/com/soywiz/korio/async/runBlockingNoJs.kt
+++ korio/src/androidMain/kotlin/com/soywiz/korio/async/runBlockingNoJs.kt
@@ -3,6 +3,6 @@ package com.soywiz.korio.async
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
 
-actual fun <T> runBlockingNoJs(context: CoroutineContext, block: suspend CoroutineScope.() -> T): T {
-    return runBlocking { block() }
-}
+@OptIn(ExperimentalStdlibApi::class)
+actual fun <T> runBlockingNoJs(context: CoroutineContext, block: suspend CoroutineScope.() -> T): T =
+    runBlocking(context.minusKey(CoroutineDispatcher.Key)) { block() }
