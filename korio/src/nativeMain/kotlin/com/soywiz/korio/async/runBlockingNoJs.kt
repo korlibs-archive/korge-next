6ca99bcacd7383dc75dd5c04f15d0e383a31360d
soywiz <soywiz@gmail.com>
2022-05-30 20:50:29 +0200

    Do not include Job or Dispatcher in runBlockingNoJs

--- korio/src/nativeMain/kotlin/com/soywiz/korio/async/runBlockingNoJs.kt
+++
@@ -1,8 +0,0 @@
-package com.soywiz.korio.async
-
-import kotlinx.coroutines.*
-import kotlin.coroutines.*
-
-@OptIn(ExperimentalStdlibApi::class)
-actual fun <T> runBlockingNoJs(context: CoroutineContext, block: suspend CoroutineScope.() -> T): T =
-    runBlocking(context.minusKey(CoroutineDispatcher.Key)) { block() }
--- korio/src/nativeMain/kotlin/com/soywiz/korio/async/runBlockingNoJsNative.kt
+++ korio/src/nativeMain/kotlin/com/soywiz/korio/async/runBlockingNoJsNative.kt
@@ -3,6 +3,5 @@ package com.soywiz.korio.async
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
 
-@OptIn(ExperimentalStdlibApi::class)
 actual fun <T> runBlockingNoJs(context: CoroutineContext, block: suspend CoroutineScope.() -> T): T =
-    runBlocking(context.minusKey(CoroutineDispatcher.Key)) { block() }
+    runBlocking(runBlockingNoJs_transformContext(context)) { block() }
