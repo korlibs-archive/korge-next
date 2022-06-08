da473b36af3f461a4d0c70705dec7bf0bd8f0458
soywiz <soywiz@gmail.com>
2022-06-08 19:48:10 +0200

    Fix obtaining DPI on Windows

--- korgw/src/mingwMain/kotlin/com/soywiz/korgw/Win32AGOpengl.kt
+++ korgw/src/mingwMain/kotlin/com/soywiz/korgw/Win32AGOpengl.kt
@@ -22,19 +22,41 @@ import platform.windows.MonitorFromWindow
 
 class Win32AGOpengl(val hwnd: () -> HWND?) : AGNative() {
     override val pixelsPerInch: Double get() {
-        val monitor = MonitorFromWindow(hwnd(), MONITOR_DEFAULTTONEAREST)
-        memScoped {
+        try {
+            return kotlin.math.max(pixelsPerInchRaw, 96.0)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            return 96.0
+        }
+    }
+    val pixelsPerInchRaw: Double get() = memScoped {
+        val hwnd = hwnd()
+
+        // Windows 8.1
+        if (GetDpiForMonitor != null) {
+            val monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST)
             val dpiX = alloc<UIntVar>()
             val dpiY = alloc<UIntVar>()
-            if (GetDpiForMonitor != null) {
-                GetDpiForMonitor?.invoke(monitor, MDT_RAW_DPI, dpiX.ptr, dpiY.ptr)
+            val result = GetDpiForMonitor!!(monitor, MDT_RAW_DPI, dpiX.ptr, dpiY.ptr)
+            //println("GetDpiForMonitor: hwnd=$hwnd, result=$result, monitor=$monitor, dpiX=${dpiX.value}")
+            if (result == 0) {
                 return dpiX.value.toDouble()
-            } else {
-                // @TODO: Use monitor information:
-                // @TODO: https://stackoverflow.com/questions/577736/how-to-obtain-the-correct-physical-size-of-the-monitor
-                return 96.0
             }
         }
+
+        // Windows 10 only or greater
+        if (GetDpiForWindow != null) {
+            // This usually returns 96.0 or other thing if scaled
+            val dpi = GetDpiForWindow!!(hwnd).toDouble()
+            //println("GetDpiForWindow.dpi: hwnd=$hwnd, $dpi")
+            if (dpi >= 96.0) return dpi
+        }
+
+        // Older Windows Versions
+
+        // @TODO: Use monitor information:
+        // @TODO: https://stackoverflow.com/questions/577736/how-to-obtain-the-correct-physical-size-of-the-monitor
+        return 96.0
     }
 
     companion object {
@@ -46,6 +68,10 @@ class Win32AGOpengl(val hwnd: () -> HWND?) : AGNative() {
         private val shCore by lazy { LoadLibraryA("Shcore.dll") }
         private val GetDpiForMonitor: CPointer<CFunction<(hmonitor: HMONITOR?, dpiType: Int, dpiX: CPointer<UIntVar>, dpiY: CPointer<UIntVar>) -> Int>>? by lazy {
             GetProcAddress(shCore, "GetDpiForMonitor")?.reinterpret()
+        }
+        private val user32 by lazy { LoadLibraryA("user32.dll") }
+        private val GetDpiForWindow: CPointer<CFunction<(hwnd: HWND?) -> Int>>? by lazy {
+            GetProcAddress(user32, "GetDpiForWindow")?.reinterpret()
         }
     }
 }
