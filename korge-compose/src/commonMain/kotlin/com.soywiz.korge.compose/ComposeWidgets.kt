acea5f9145e5523ecae63f5aacd0b7bac2a1c9df
soywiz <soywiz@gmail.com>
2022-06-10 04:57:23 +0200

    More work

--- korge-compose/src/commonMain/kotlin/com.soywiz.korge.compose/ComposeWidgets.kt
+++ korge-compose/src/commonMain/kotlin/com.soywiz.korge.compose/ComposeWidgets.kt
@@ -1,10 +1,15 @@
 package com.soywiz.korge.compose
 
 import androidx.compose.runtime.*
+import com.soywiz.korev.Key
 import com.soywiz.korge.input.*
 import com.soywiz.korge.ui.*
+import com.soywiz.korge.view.DummyView
+import com.soywiz.korge.view.addUpdater
+import com.soywiz.korge.view.vector.GpuShapeView
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RGBA
+import com.soywiz.korim.vector.Context2d
 
 @Composable
 fun Text(text: String, color: RGBA = Colors.WHITE, onClick: () -> Unit = {}) {
@@ -42,3 +47,32 @@ fun VStack(content: @Composable () -> Unit) {
 fun HStack(content: @Composable () -> Unit) {
     ComposeNode<UIHorizontalStack, NodeApplier>(::UIHorizontalStack, {}, content)
 }
+
+@Composable
+fun KeyDown(key: Key, onPress: (Key) -> Unit = {}) {
+    KeyDown { if (it == key) onPress(it) }
+}
+
+@Composable
+fun KeyDown(onPress: (Key) -> Unit = {}) {
+    ComposeNode<DummyView, NodeApplier>({
+        DummyView()
+    }) {
+        set(onPress) { this.keys.down { onPress(it.key) } }
+    }
+}
+
+@Composable
+fun Canvas(onDraw: Context2d.() -> Unit = {}) {
+    ComposeNode<GpuShapeView, NodeApplier>({
+        GpuShapeView().also {
+            it.updateShape { onDraw() }
+        }.also {
+            it.addUpdater { dt ->
+                it.updateShape { onDraw() }
+            }
+        }
+    }) {
+    }
+}
+
--- korge-sandbox/src/commonMain/kotlin/MainComposable.kt
+++ korge-sandbox/src/commonMain/kotlin/MainComposable.kt
@@ -1,21 +1,30 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeNode
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.soywiz.klock.milliseconds
-import com.soywiz.korge.Korge
+import com.soywiz.korev.Key
 import com.soywiz.korge.compose.Button
 import com.soywiz.korge.compose.HStack
+import com.soywiz.korge.compose.NodeApplier
 import com.soywiz.korge.compose.Text
 import com.soywiz.korge.compose.VStack
 import com.soywiz.korge.compose.setComposeContent
-import com.soywiz.korge.time.delay
+import com.soywiz.korge.input.keys
+import com.soywiz.korge.view.DummyView
 import com.soywiz.korge.view.Stage
+import com.soywiz.korge.view.View
+import com.soywiz.korge.view.ViewRenderPhase
+import com.soywiz.korge.view.addUpdater
+import com.soywiz.korge.view.vector.GpuShapeView
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.interpolate
+import com.soywiz.korim.vector.Context2d
 import com.soywiz.korio.async.delay
+import com.soywiz.korma.geom.vector.roundRect
 import kotlin.coroutines.cancellation.CancellationException
 
 suspend fun Stage.mainComposable() {
@@ -28,13 +37,15 @@ suspend fun Stage.mainComposable() {
 fun App() {
     var color by remember { mutableStateOf(Colors.RED) }
     var count by remember { mutableStateOf(0) }
+    var ratio by remember { mutableStateOf(0.0) }
     LaunchedEffect(count) {
         println("LaunchedEffect=$count..started")
         try {
             val nsteps = 20
             for (n in 0..nsteps) {
-                val ratio = n.toDouble() / nsteps.toDouble()
-                color = ratio.interpolate(Colors.RED, Colors.WHITE)
+                val r = n.toDouble() / nsteps.toDouble()
+                ratio = r
+                color = r.interpolate(Colors.RED, Colors.WHITE)
                 delay(10.milliseconds)
             }
             println("LaunchedEffect=$count..ended")
@@ -49,5 +60,12 @@ fun App() {
             Button("-") { count-- }
             Button("+") { count++ }
         }
+        Canvas {
+            fill(color) {
+                roundRect(0.0, 0.0, 100.0, 100.0, 20 * ratio, 20 * ratio)
+            }
+        }
     }
+    KeyDown(Key.DOWN) { count-- }
+    KeyDown(Key.UP) { count++ }
 }
