f0a214f83d5c74c71ed7971b5f14aa01bb1f0aca
soywiz-invideo <soywiz@invideo.io>
2022-06-15 12:50:36 +0200

    Optimize and document LineCap

--- korma/src/commonMain/kotlin/com/soywiz/korma/geom/vector/LineCap.kt
+++ korma/src/commonMain/kotlin/com/soywiz/korma/geom/vector/LineCap.kt
@@ -1,17 +1,50 @@
 package com.soywiz.korma.geom.vector
 
+/**
+ * Determines how the lines end or start
+ */
 enum class LineCap {
+    /**
+     * Creates a butt cap, keeping
+     * the size of the path.
+     *
+     *  ┌───────
+     *  │┈┈┈┈┈┈┈
+     *  └───────
+     */
     BUTT,
+
+    /**
+     * Creates a square cap, expanding
+     * the size of the path.
+     *
+     *  ┌─────────
+     *  │  ┈┈┈┈┈┈┈
+     *  └─────────
+     */
     SQUARE,
+    /**
+     * Creates a rounded circular cap,
+     * expanding the size of the path.
+     *
+     *  ╭─────────
+     *  │  ┈┈┈┈┈┈┈
+     *  ╰─────────
+     *
+     * Note:
+     * The roundness of the figure is limited by the characters used.
+     */
     ROUND;
 
     companion object {
-        operator fun get(str: String?): LineCap = when (str) {
-            null -> BUTT
-            "BUTT", "butt" -> BUTT
-            "SQUARE", "square" -> SQUARE
-            "ROUND", "round" -> ROUND
-            else -> BUTT
+        operator fun get(str: String?): LineCap = when {
+            str.isNullOrEmpty() -> BUTT
+            else -> when (str[0].uppercaseChar()) {
+                'B' -> BUTT
+                'S' -> SQUARE
+                'R' -> ROUND
+                else -> BUTT
+            }
         }
     }
 }
