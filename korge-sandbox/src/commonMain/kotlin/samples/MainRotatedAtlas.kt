34cd5f14e09ea4bc6b5d0294574b2d048fbeeb20
Stechmuck <stechi@famro.de>
2022-06-16 19:11:52 +0200

    Update AtlasRegion with current implementation

--- korge-sandbox/src/commonMain/kotlin/samples/MainRotatedAtlas.kt
+++ korge-sandbox/src/commonMain/kotlin/samples/MainRotatedAtlas.kt
@@ -3,88 +3,83 @@ package samples
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.Container
 import com.soywiz.korge.view.image
+import com.soywiz.korge.view.solidRect
 import com.soywiz.korge.view.xy
 import com.soywiz.korim.atlas.Atlas
-import com.soywiz.korim.atlas.AtlasInfo
-import com.soywiz.korim.bitmap.sliceWithSize
-import com.soywiz.korim.format.readBitmap
+import com.soywiz.korim.atlas.readAtlas
+import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korim.bitmap.BitmapSlice
+import com.soywiz.korim.bitmap.asBitmapSlice
+import com.soywiz.korim.bitmap.virtFrame
+import com.soywiz.korim.color.RGBA
+import com.soywiz.korim.format.ImageOrientation
 import com.soywiz.korio.file.std.resourcesVfs
-import com.soywiz.korio.lang.Charsets
-import com.soywiz.korio.serialization.xml.Xml
+import kotlin.math.max
+import kotlin.native.concurrent.ThreadLocal
 
 class MainRotatedAtlas : Scene() {
     override suspend fun Container.sceneMain() {
-        var atlas = parseAtlas("test.xml")
-        var x = 0.0
-        for (i in 1..7) {
-            image(atlas["$i"]).xy(x, 0.0).also {
-                x += it.width
+        var y = 0
+
+        fun generateImages(atlas: Atlas, ext: String = "") {
+            var x = 0
+            var maxY = 0
+            for (i in 1..7) {
+                val bmp = atlas["$i$ext"]
+                solidRect(bmp.frameWidth, bmp.frameHeight, BG.color).xy(x, y)
+                image(bmp).xy(x, y).also {
+                    x += it.frameWidth.toInt()
+                    maxY = max(maxY, it.frameHeight.toInt())
+                }
+            }
+            y += maxY
+        }
+
+        fun generateImages(atlas: Atlas) {
+            var x = 0
+            val atlas1 = atlas["atlas1"]
+            val atlas2 = atlas["atlas2"]
+            val atlas3 = atlas["atlas3"]
+            for (i in 1..6) {
+                solidRect(100, 100, BG.color).xy(x, y)
+                image(when(i) {
+                    1 -> atlas1.sliceWithSize(0, 0, 50, 60, "a$i").asBitmapSlice<Bitmap>().virtFrame(25, 20, 100, 100)
+                    2 -> atlas1.sliceWithSize(50, 0, 50, 60, "a$i").asBitmapSlice<Bitmap>().virtFrame(25, 20, 100, 100)
+                    3 -> atlas2.sliceWithSize(0, 0, 50, 60, "a$i").asBitmapSlice<Bitmap>().virtFrame(25, 20, 100, 100)
+                    4 -> atlas2.sliceWithSize(50, 0, 50, 60, "a$i").asBitmapSlice<Bitmap>().virtFrame(25, 20, 100, 100)
+                    5 -> atlas3.sliceWithSize(0, 0, 100, 10, "a$i").asBitmapSlice<Bitmap>().virtFrame(0, 45, 100, 100)
+                    else -> (atlas3.sliceWithSize(0, 10, 10, 100, "a$i", ImageOrientation.ROTATE_270) as BitmapSlice<Bitmap>).virtFrame(45, 0, 100, 100)
+                }).xy(x, y).also {
+                    if (i == 3) {
+                        x = 0
+                        y += 100
+                    } else {
+                        x += it.frameWidth.toInt()
+                    }
+                }
             }
         }
 
-        atlas = parseAtlas("test.xml", true)
-        x = 0.0
-        for (i in 1..7) {
-            image(atlas["$i"]).xy(x, 100.0).also {
-                x += it.width
-            }
-        }
-    }
+        generateImages(resourcesVfs["atlastest/atlas-test.xml"].readAtlas(), "")
 
-    suspend fun parseAtlas(atlasFile: String, applyRotation: Boolean = false): Atlas {
-        val xmlContent = resourcesVfs[atlasFile].readString(Charsets.UTF8)
-        val xml = Xml(xmlContent)
-        val imagePath = xml.str("imagePath")
+        generateImages(resourcesVfs["atlastest/atlas-test-spine.atlas.txt"].readAtlas(), "")
 
-        val atlasImg = resourcesVfs[imagePath].readBitmap().sliceWithSize(0, 0, xml.int("width"), xml.int("height"))
-        val atlasRegions = mutableListOf<AtlasInfo.Region>()
+        generateImages(resourcesVfs["atlastest/atlas-test.json"].readAtlas(), ".png")
 
-        xml.children("SubTexture").forEach {
-            val name = it.str("name", "")
-            if (name.isEmpty()) {
-                return@forEach
-            }
-            val rotated = it.boolean("rotated", false)
-            val texFrame = AtlasInfo.Rect(
-                it.int("x"),
-                it.int("y"),
-                it.int("width"),
-                it.int("height")
-            )
-            val trimmed = it.hasAttribute("frameX")
-            val imageFrame =
-                if (trimmed) AtlasInfo.Rect(
-                    it.int("frameX") * -1,
-                    it.int("frameY") * -1,
-                    it.int("frameWidth"),
-                    it.int("frameHeight")
-                )
-                else texFrame
-
-            val region = AtlasInfo.Region(
-                name = name,
-                frame = texFrame,
-                rotated = rotated,
-                sourceSize = AtlasInfo.Size(imageFrame.w, imageFrame.h),
-                spriteSourceSize = imageFrame,
-                orig = AtlasInfo.Size(imageFrame.w, imageFrame.h),
-                trimmed = trimmed
-            )
-
-            atlasRegions += if (applyRotation) region.applyRotation() else region
-        }
-
-        val atlasInfo = AtlasInfo(
-            frames = atlasRegions,
-            meta = AtlasInfo.Meta(
-                app = "Sparrow/Starling",
-                format = "RGBA8888",
-                image = imagePath,
-                scale = 1.0,
-                size = AtlasInfo.Size(xml.int("width"), xml.int("height")),
-                version = AtlasInfo.Meta.VERSION
-            )
-        )
-        return Atlas(atlasImg, atlasInfo)
+        generateImages(resourcesVfs["atlastest/atlas.xml"].readAtlas())
     }
 }
+
+class BG {
+    @ThreadLocal
+    companion object {
+        private val colAr = arrayOf(RGBA(240, 240, 240), RGBA(220, 220, 220))
+        private var cur = true
+        val color: RGBA
+            get() {
+                cur = !cur
+                return colAr[if (cur) 1 else 0]
+            }
+    }
+}
+
